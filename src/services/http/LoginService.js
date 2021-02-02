0fffa22822e7903870b29b652f08391acf8a976c
Jack <14117126+jdipper@users.noreply.github.com>
2021-02-02 15:49:32 +0000

    Updated submodules

---
+++ src/services/http/LoginService.js
@@ -0,0 +1,41 @@
+import http from "../../ReactHelpers/http/http-common";
+
+/*const getAll = () => {
+  return http.get("/contacts");
+};
+
+const get = id => {
+  return http.get(`/contacts/${id}`);
+};
+
+const create = data => {
+  return http.post("/contacts", data);
+};
+
+const update = (id, data) => {
+  return http.put(`/contacts/${id}`, data);
+};
+
+const remove = id => {
+  return http.delete(`/contacts/${id}`);
+};
+
+const removeAll = () => {
+  return http.delete(`/contacts`);
+};
+
+const findByTitle = title => {
+  return http.get(`/contacts?title=${title}`);
+};*/
+
+const LoginService = {
+  /*getAll,
+  get,
+  create,
+  update,
+  remove,
+  removeAll,
+  findByTitle*/
+};
+
+export default LoginService
