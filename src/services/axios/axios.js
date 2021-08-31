0da5291b09b59c60cbfba43de0953f3e25277658
Jack <14117126+jdipper@users.noreply.github.com>
2021-08-31 18:06:55 +0000

    Force no cache on axios request

--- src/services/axios/axios.js
+++ src/services/axios/axios.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const instance = axios.create({
   headers: {
+    common: {
+      "Cache-Control": "no-cache",
+      "Pragma": "no-cache",
+      "Expires": "0"
+    },
     post: {
       "Content-Type": "application/json",
     },
