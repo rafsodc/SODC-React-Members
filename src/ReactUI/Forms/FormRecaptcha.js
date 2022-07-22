e0fd82c12764a7abe5bbda57fe3e99e148721b96
Jack <14117126+jdipper@users.noreply.github.com>
2022-07-22 17:18:56 +0100

    Removed hard coding of recaptcha site key

--- src/ReactUI/Forms/FormRecaptcha.js
+++ src/ReactUI/Forms/FormRecaptcha.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import {Form} from "react-bootstrap";
+import config from "../../config/config";
 
 
 const FormRecaptcha = (props) => (
   <Form.Group controlId="formCaptcha">
     <ReCAPTCHA className={props.error ? "form-warning-captcha" : null}
-               sitekey="6LfJ4jwbAAAAABkOxONVVddw81bLQrqo5JCiBC5_"
+               sitekey={config.captcha.sitekey}
                onChange={props.onRecaptcha}
     />
     <Form.Text muted className={"form-warning-desc"}>{props.error && "reCAPTCHA needs to be completed"}</Form.Text>
