f11e824c95a340e5489fd261087cf9b3f6d123a4
Jack <14117126+jdipper@users.noreply.github.com>
2023-04-15 21:50:26 +0100

    Clean up of code in src/containers/Event/EventPage

--- src/containers/Event/EventPage.js
+++ src/containers/Event/EventPage.js
@@ -1,40 +1,39 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { loadEvent } from '../../store/actions/event'
-import Aux from '../../hoc/Aux'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {loadEvent} from '../../store/actions/event'
 import Event from './Event'
-import { Accordion } from 'react-bootstrap'
+import {Accordion} from 'react-bootstrap'
 import Loading from '../../components/Loading/Loading'
-import { useParams } from 'react-router'
+import {useParams} from 'react-router'
 import Booking from '../Booking/Booking'
 import BookingOwnerSelect from '../Booking/BookingOwnerSelect'
 
 const EventPage = () => {
 
-  const dispatch = useDispatch()
-  const eventState = useSelector(state => state.eventReducer)
+    const dispatch = useDispatch()
+    const {event, ownerSelectDisabled} = useSelector(state => state.eventReducer)
+    const {id} = useParams()
 
-  const { id } = useParams()
+    useEffect(() => {
+        dispatch(loadEvent(`/events/${id}`))
+    }, [dispatch, id])
 
-  useEffect(() => {
-    dispatch(loadEvent(`/events/${id}`))
-  }, [dispatch])
+    if (!event) {
+        return <Loading/>
+    }
 
-  if (eventState.event !== null) {
-    return <Aux>
-      <h2>Event Booking</h2>
-      <Accordion>
-        <Event {...eventState.event} showTickets={false} eventKey={'event'}/>
-      </Accordion>
-      <br/>
-      <BookingOwnerSelect disabled={eventState.ownerSelectDisabled}>
-        <Booking ticketOptions={eventState.event.ticketTypes} event={`/events/${id}`}/>
-      </BookingOwnerSelect>
-    </Aux>
-  } else {
-    return <Loading/>
-  }
-
+    return (
+        <>
+            <h2>Event Booking</h2>
+            <Accordion>
+                <Event {...event} showTickets={false} eventKey={'event'}/>
+            </Accordion>
+            <br/>
+            <BookingOwnerSelect disabled={ownerSelectDisabled}>
+                <Booking ticketOptions={event.ticketTypes} event={`/events/${id}`}/>
+            </BookingOwnerSelect>
+        </>
+    )
 }
 
 export default EventPage
