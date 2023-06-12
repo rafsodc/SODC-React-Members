bb6a7a5b5b9f5f95d78b81325021d9b8445a4a85
Jack <14117126+jdipper@users.noreply.github.com>
2023-06-12 11:57:23 +0100

    Added edit form for Events

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import AuthLink from "../../components/Links/AuthLink";
-import {APP_USERS_APPROVE} from "../../components/Routes/RouteDefs";
+import {APP_EVENT_ADD, APP_USERS_APPROVE} from "../../components/Routes/RouteDefs";
 
 const Admin = () => {
 
@@ -10,6 +10,7 @@ const Admin = () => {
     return (
         <>
             <h2>Website Admin</h2>
+            <AuthLink route={APP_EVENT_ADD} userRoles={authState.token_data.roles}/>
             <AuthLink route={APP_USERS_APPROVE} userRoles={authState.token_data.roles}/>
         </>
     )
