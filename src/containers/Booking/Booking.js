813fdb65968092321c073272cd201228af82f42c
Jack <14117126+jdipper@users.noreply.github.com>
2024-07-28 17:25:42 +0100

    Fix error with status check

--- src/containers/Booking/Booking.js
+++ src/containers/Booking/Booking.js
@@ -25,8 +25,7 @@ const Booking = (props) => {
     //dispatch(loadTransactions(props.event, bookingState.owner))
     
     // Determine status is set.  If so, display the confirmation tab.
-    console.log(status !== undefined);
-    if (status !== null) handleTabSelect('confirmation')
+    if (status !== undefined) handleTabSelect('confirmation')
 
   }, [dispatch, bookingState.owner, props.event, status])
 
